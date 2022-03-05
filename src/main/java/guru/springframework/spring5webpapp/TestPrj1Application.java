5da503ac432454729de933567888ae556cb5e637
hyunfromseoul <hongjae30@gmail.com>
2022-03-05 12:37:29 +0900

    #1. 첫번째 커밋

---
+++ src/main/java/guru/springframework/spring5webpapp/TestPrj1Application.java
@@ -0,0 +1,13 @@
+package guru.springframework.spring5webpapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestPrj1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestPrj1Application.class, args);
+	}
+
+}
---
+++ src/test/java/guru/springframework/spring5webpapp/TestPrj1ApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.springframework.spring5webpapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestPrj1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
